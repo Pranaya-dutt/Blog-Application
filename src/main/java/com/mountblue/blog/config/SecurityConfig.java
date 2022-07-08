320fd680a18c33687538dc70260b1c07f356a1f2
Pranaya Dutt <pranayadutt2000@gmail.com>
2022-07-08 19:10:48 +0530

    Replaced deprecated security methods

--- src/main/java/com/mountblue/blog/config/SecurityConfig.java
+++ src/main/java/com/mountblue/blog/config/SecurityConfig.java
@@ -1,26 +1,21 @@
 package com.mountblue.blog.config;
 
-import com.mountblue.blog.service.CustomUserDetailService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private CustomUserDetailService customUserDetailService;
+public class SecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/newpost","/updatePost/**","/deletePost/**","/showPost/{id}/updateComment/{commentId}/**", "/showPost/{postId}/deleteComment/**").authenticated()
@@ -38,13 +33,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/");
+        return httpSecurity.build();
     }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(customUserDetailService).passwordEncoder(bCryptPasswordEncoder());
-    }
-
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder(){
         return new BCryptPasswordEncoder(10);
