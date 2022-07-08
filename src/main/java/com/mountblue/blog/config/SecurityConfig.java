3fdbd2a86ccfae5dead0eb5f34c56c4084a3e93d
Pranaya Dutt <pranayadutt2000@gmail.com>
2022-07-08 19:03:32 +0530

    Replaced depricated security method

--- src/main/java/com/mountblue/blog/config/SecurityConfig.java
+++ src/main/java/com/mountblue/blog/config/SecurityConfig.java
@@ -9,22 +9,18 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private CustomUserDetailService customUserDetailService;
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
                 .authorizeRequests()
                 .antMatchers("/newpost","/updatePost/**","/deletePost/**","/showPost/{id}/updateComment/{commentId}/**", "/showPost/{postId}/deleteComment/**").authenticated()
                 .and()
-//                .exceptionHandling().accessDeniedPage("/accessDenied")
-//                .and()
                 .formLogin()
                 .loginPage("/signin")
                 .loginProcessingUrl("/dologin")
@@ -32,13 +28,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/");
+        return httpSecurity.build();
     }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(customUserDetailService).passwordEncoder(bCryptPasswordEncoder());
-    }
-
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder(){
         return new BCryptPasswordEncoder(10);
